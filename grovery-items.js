c2aca7b2bd932e7af9a1d13c5b26436613c36ce5
Daniel Sochor <danielsochor@gmail.com>
2020-04-11 12:38:17 -0500

    add grocery list js file

---
+++ grovery-items.js
@@ -0,0 +1,107 @@
+var groceryItems = [
+        {
+            question: "We can hook onto the event of a user pressing and releasing a key by referring to ____.",
+            options: [
+                {
+                    number: 0,
+                    answer: "event.key"
+                },
+                {
+                    number: 1,
+                    answer: "document.onkeyup"
+                },
+                {
+                    number: 2,
+                    answer: "source code"
+                },
+                {
+                    number: 3,
+                    answer: "selector"
+                }],
+            answer: 1
+        },
+        {
+            question: "We can get the key a user has pressed by referencing ____.",
+            options: [
+                {
+                    number: 0,
+                    answer: "event.key"
+                },
+                {
+                    number: 1,
+                    answer: "document.onkeyup"
+                },
+                {
+                    number: 2,
+                    answer: "selector"
+                },
+                {
+                    number: 3,
+                    answer: "source code"
+                }],
+            answer: 0
+        },
+        {
+            question: "A function's ____ are only visble inside the function body.",
+            options: [
+                {
+                    number: 0,
+                    answer: "parameters"
+                },
+                {
+                    number: 1,
+                    answer: "variables"
+                },
+                {
+                    number: 2,
+                    answer: "defitions"
+                },
+                {
+                    number: 3,
+                    answer: "arguments"
+                }],
+            answer: 3
+        },
+        {
+            question: "Which is not an cornerstone technology of the World Wide Web?",
+            options: [
+                {
+                    number: 0,
+                    answer: "HTML"
+                },
+                {
+                    number: 1,
+                    answer: "CSS"
+                },
+                {
+                    number: 2,
+                    answer: "SQL"
+                },
+                {
+                    number: 3,
+                    answer: "JavaScript"
+                }],
+            answer: 2
+        },
+        {
+            question: "To grab the first letter of a string we can use the ____ method",
+            options: [
+                {
+                    number: 0,
+                    answer: "chatAt(0)"
+                },
+                {
+                    number: 1,
+                    answer: "scientific"
+                },
+                {
+                    number: 2,
+                    answer: "SQL"
+                },
+                {
+                    number: 3,
+                    answer: "JavaScript"
+                }],
+            answer: 2
+        }
+    ]
